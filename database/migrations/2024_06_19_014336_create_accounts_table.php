fa743c12bbeed74b69ffbd54edbe716a33438574
callmePluto <anucha.se@rmuti.ac.th>
2024-06-19 16:10:00 +0700

    update : schema

--- database/migrations/2024_06_19_014336_create_accounts_table.php
+++ database/migrations/2024_06_19_014336_create_accounts_table.php
@@ -7,7 +7,7 @@ use Illuminate\Support\Facades\Schema;
 return new class extends Migration
 {
     /**
-     * Run the migrations.
+     * Run the migrationsss.
      */
     public function up(): void
     {
