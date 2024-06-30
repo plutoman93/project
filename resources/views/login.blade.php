e298be8242a3d414f71e604b2e167e54bb4fe02a
beyond <gongpipop.2546@gmail.com>
2024-06-30 12:42:15 +0700

    css login

---
+++ resources/views/login.blade.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+
+    <link rel="stylesheet" href="{{ asset('asset/frontend/css/style1.css') }}">
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+</head>
+<body>
+    <div class="wrapper">
+        <h1>Login</h1>
+        <div class="input-box">
+            <input type="text" placeholder="Username"
+            required>
+            <i class='bx bxs-user'></i>
+        </div>
+        <div class="input-box">
+            <input type="password" placeholder="Password"
+            required>
+            <i class='bx bxs-lock-alt' ></i>
+        </div>
+
+        <div class="remember-forgot">
+            <label>
+                <input type="checkbox">จดจำรหัสผ่าน
+            </label>
+            <a href="#">ลืมรหัสผ่าน</a>
+        </div>
+
+        <button type="submit" class="btn">เข้าสู่ระบบ</button>
+    </div>
+</body>
+</html>
