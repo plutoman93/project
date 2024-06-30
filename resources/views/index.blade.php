f6c36d68391126c45aa2903129218ab527d80e62
beyond <gongpipop.2546@gmail.com>
2024-07-01 00:28:40 +0700

    login

---
+++ resources/views/index.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+
+    <link rel="stylesheet" href="{{ asset('asset/frontend/css/style2.css') }}">
+</head>
+<body>
+    <nav>
+        <div class="nav-container">
+            <a href="index.html">
+                <img src="asset/frontend/images/logos/logo_RMUTI.png" class="logonav">
+            </a>
+        </div>
+    </nav>
+</body>
+</html>
