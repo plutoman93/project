765b48c43bb68e04f12562d0a02e0676dceea08b
callmePluto <anucha.se@rmuti.ac.th>
2024-07-01 15:40:32 +0700

    test

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Login</title>
-
+{{-- 55 --}}
     <link rel="stylesheet" href="{{ asset('asset/frontend/css/style1.css') }}">
     <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
 </head>
