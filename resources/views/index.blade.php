299b67da7392675ad2eee038710cfdc1191b2dee
beyond <gongpipop.2546@gmail.com>
2024-07-08 19:39:05 +0700

    a

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>Home Page</title>
 
     <link rel="stylesheet" href="{{ asset('asset/frontend/css/style2.css') }}">
 </head>
