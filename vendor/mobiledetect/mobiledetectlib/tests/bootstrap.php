3b0a014764f99994d71a76c8871075289cf74ac4
callmePluto <anucha.se@rmuti.ac.th>
2024-08-26 22:50:55 +0700

    delete.

--- vendor/mobiledetect/mobiledetectlib/tests/bootstrap.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-require __DIR__ . '/../vendor/autoload.php';
-
-require_once dirname(__FILE__) . '/../src/MobileDetect.php';
-require_once dirname(__FILE__) . '/../src/Cache/CacheItem.php';
-require_once dirname(__FILE__) . '/../src/Cache/Cache.php';
-require_once dirname(__FILE__) . '/../src/Cache/CacheException.php';
-require_once dirname(__FILE__) . '/../src/Exception/MobileDetectException.php';
