6551ab854b046c6cdf42872fcefe81bbd1ccc59b
beyond <gongpipop.2546@gmail.com>
2024-09-05 19:18:13 +0700

    .

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -11,7 +11,7 @@
 
 <head>
     <meta charset="utf-8">
-    <title>LoginPage Demo mini project</title>
+    <title>WIMS.LoginPage</title>
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
 
@@ -74,14 +74,17 @@
                                         placeholder="Password" required>
                                 </div>
                             </div>
+
                             <div class="col-md-12.5">
                                 <button class="btn btn-primary" type="submit">Login</button>
 
-                                <p class="mt-3 mb-1">Not a member yet? <a href="{{ route('register') }}">Register
-                                        Here</a></p>
-                                <p class="mt-3 mb-1">Forgot Password<a href="{{ route('password.request') }}">Forgot
-                                        Password</a></p>
                             </div>
+                            {{-- <div class="regis">
+                                <p class="mt-3 mb-1">Not a member yet? <a href="{{ route('register') }}">Register
+                                    Here</a></p>
+                            <p class="mt-3 mb-1">Forgot Password<a href="{{ route('password.request') }}">Forgot
+                                    Password</a></p>
+                            </div> --}}
                         </form>
                     </div>
                 </div>
