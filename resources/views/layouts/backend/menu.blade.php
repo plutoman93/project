9532f792dc085a92cf466ea126da8821bcc2201b
beyond <gongpipop.2546@gmail.com>
2024-09-05 20:25:30 +0700

    .

--- resources/views/layouts/backend/menu.blade.php
+++ resources/views/layouts/backend/menu.blade.php
@@ -1,8 +1,8 @@
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
     <a href="index3.html" class="brand-link">
-      <img src="asset/backend/dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
-      <span class="brand-text font-weight-light">WMS</span>
+      <img src="asset/frontend/images/logos/logo_RMUTI.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+      <span class="brand-text font-weight-light">WIMS.TC</span>
     </a>
 
     <!-- Sidebar -->
