a9d76f2a2c4171df345d981fbc3cc7646b6ed526
callmePluto <anucha.se@rmuti.ac.th>
2024-09-06 12:45:15 +0700

    ลบ

--- resources/views/livewire/addtask.blade.php
+++ resources/views/livewire/addtask.blade.php
@@ -29,7 +29,6 @@
                         <div class="card card-primary">
                             <div class="card-header">
                                 <h3 class="card-title">จัดการการเพิ่มข้อมูลงาน</h3>
-                                <h3 class="card-title">General</h3>
                                 <div class="card-tools">
                                     <button type="button" class="btn btn-tool" data-card-widget="collapse"
                                         title="Collapse">
