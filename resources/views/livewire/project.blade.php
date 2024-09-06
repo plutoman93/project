2141f72944cd26e2edc2df3e4de4e4a2d7e0495f
beyond <gongpipop.2546@gmail.com>
2024-09-06 13:14:27 +0700

    ลบ copyright

--- resources/views/livewire/project.blade.php
+++ resources/views/livewire/project.blade.php
@@ -230,17 +230,6 @@
 
             </div>
             <!-- End of Main Content -->
-
-            <!-- Footer -->
-            <footer class="sticky-footer bg-white">
-                <div class="container my-auto">
-                    <div class="copyright text-center my-auto">
-                        <span>Copyright &copy; Work Informatiom Management System</span>
-                    </div>
-                </div>
-            </footer>
-            <!-- End of Footer -->
-
         </div>
         <!-- End of Content Wrapper -->
 
