8bfa70290629114877a8e07b3fadf0db18a97c8c
callmePluto <anucha.se@rmuti.ac.th>
2024-09-07 15:28:04 +0700

    🔧 ลบ column ให้ตรงตามฟอร์มงาน

--- app/Livewire/project/Add.php
+++ app/Livewire/project/Add.php
@@ -8,7 +8,7 @@ class Add extends Component // เปลี่ยน Addtask เป็น AddTas
 {
     use WithFileUploads;
 
-    public $task_name, $task_detail, $start_date, $due_date, $file, $task_type, $status_task;
+    public $task_name, $task_detail, $start_date, $due_date, $file, $task_type; //ลบ status task เพื่อให้เก็บข้อมูลได้ตามฟอร์มที่กำหนด
 
     public function add()
     {
@@ -19,7 +19,7 @@ class Add extends Component // เปลี่ยน Addtask เป็น AddTas
             'start_date' => 'required|date',
             'due_date' => 'required|date',
             'task_type' => 'required',
-            'status_task' => 'required',
+            // 'status_task' => 'required', //ลบ status task เพื่อให้เก็บข้อมูลได้ตามฟอร์มที่กำหนด
         ],[
             'task_name.required' => "กรุณากรอกชื่องาน",
             'task_name.min' => "ชื่องานต้องมีมากกว่า 3 ตัวอักษร",
@@ -34,7 +34,7 @@ class Add extends Component // เปลี่ยน Addtask เป็น AddTas
                 'start_date' => $this->start_date,
                 'due_date' => $this->due_date, //$this-> คำสั่งในการเข้าถึงตัวแปร (จำเป็นต้องตรงกันและถูกต้องตามที่ประกาศใช้งานจริง)
                 'task_type' => $this->task_type,
-                'status_task' => $this->status_task,
+                // 'status_task' => $this->status_task, //ลบ status task เพื่อให้เก็บข้อมูลได้ตามฟอร์มที่กำหนด
                 // 'created_by' => auth()->user()->id, // ยืนยันว่าใช้ auth()->user() อย่างถูกต้อง
             ]);
 
